b3f4f952baf14b013c2fddef2e53e4672d09b21f
Not-Your-Man <154847073+Not-Your-Man@users.noreply.github.com>
2024-01-26 02:02:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
 
 
 const app = express();
