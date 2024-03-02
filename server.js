54ad6cebc3b81fc8886af0ccb244a12c4423cbd2
Not-Your-Man <154847073+Not-Your-Man@users.noreply.github.com>
2024-03-02 11:36:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -482,6 +482,23 @@ app.post('/api/update-earnings', async (req, res) => {
   }
 });
 
+//FETCH EARNINGS
+// Define endpoint to fetch user earnings
+app.get('/api/earnings', async (req, res) => {
+  try {
+    const userId = req.query.userId; // Assuming userId is passed as a query parameter
+    const user = await User.findById(userId);
+    
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.status(200).json({ earnings: user.earnings });
+  } catch (error) {
+    console.error('Error fetching earnings:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 //STOP HERE
 
